8cca88f17df6092a6651b9ef6ec8e7ca7b4546a2
Hayley Witherell <hayleywitherell@gmail.com>
2024-06-10 20:25:59 -0600

    Complete lesson 6

--- lessons/06_lesson.sql
+++ lessons/06_lesson.sql
@@ -1,10 +1,21 @@
 -- SQL Lesson 6: Multi-table queries with JOINs
 
 -- 1. Find the domestic and international sales for each movie
-
+SELECT title, domestic_sales, international_sales
+FROM boxoffice
+INNER JOIN movies
+  ON boxoffice.movie_id = movies.id;
 
 -- 2. Show the sales numbers for each movie that did better internationally rather than domestically
-
+SELECT title, domestic_sales, international_sales
+FROM boxoffice
+INNER JOIN movies
+  ON boxoffice.movie_id = movies.id
+WHERE international_sales > domestic_sales;
 
 -- 3. List all the movies by their ratings in descending order
-
+SELECT title
+FROM boxoffice
+INNER JOIN movies
+  ON boxoffice.movie_id = movies.id
+ORDER BY rating DESC;
