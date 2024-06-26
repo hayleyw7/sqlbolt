01aefc4807d1ad39e925864c9114b3646a38e9eb
Hayley Witherell <hayleywitherell@gmail.com>
2024-06-26 15:50:39 -0600

    Complete lesson 18

--- lessons/18_lesson.sql
+++ lessons/18_lesson.sql
@@ -1,18 +1,7 @@
--- SQL Lesson 10: Queries with aggregates (Pt. 1)
+-- SQL Lesson 18: Dropping tables
 
--- 1. Find the longest time that an employee has been at the studio ✓
-SELECT Years_employed
-FROM employees
-ORDER BY Years_employed DESC
-LIMIT 1;
+-- 1. We've sadly reached the end of our lessons, lets clean up by removing the Movies table
+DROP TABLE movies;
 
--- 2. For each role, find the average number of years employed by employees in that role
-SELECT role, AVG(Years_employed) AS average_years_employed
-FROM employees
-GROUP BY role;
-
--- 3. Find the total number of employee years worked in each building
-SELECT building, SUM(Years_employed) AS total_years_employed
-FROM employees
-GROUP BY building;
-  
+-- 2. And drop the BoxOffice table as well
+DROP TABLE boxoffice;
