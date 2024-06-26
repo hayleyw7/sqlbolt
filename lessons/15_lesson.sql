d2951be91a25f28058739e033b4c73bc2674ec0b
Hayley Witherell <hayleywitherell@gmail.com>
2024-06-26 15:44:15 -0600

    Complete lesson 15

--- lessons/15_lesson.sql
+++ lessons/15_lesson.sql
@@ -1,18 +1,9 @@
--- SQL Lesson 10: Queries with aggregates (Pt. 1)
+-- SQL Lesson 15: Deleting rows
 
--- 1. Find the longest time that an employee has been at the studio ✓
-SELECT Years_employed
-FROM employees
-ORDER BY Years_employed DESC
-LIMIT 1;
+-- 1. This database is getting too big, lets remove all movies that were released before 2005.
+DELETE FROM movies
+WHERE year < 2005;
 
--- 2. For each role, find the average number of years employed by employees in that role
-SELECT role, AVG(Years_employed) AS average_years_employed
-FROM employees
-GROUP BY role;
-
--- 3. Find the total number of employee years worked in each building
-SELECT building, SUM(Years_employed) AS total_years_employed
-FROM employees
-GROUP BY building;
-  
+-- 2. Andrew Stanton has also left the studio, so please remove all movies directed by him.
+DELETE FROM movies
+WHERE director = 'Andrew Stanton';
